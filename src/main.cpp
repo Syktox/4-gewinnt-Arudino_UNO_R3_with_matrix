ab866a4754c5e19387e9dad63362722058f10649
Syktox <markus.kammerstetter@hotmail.com>
2023-06-02 13:08:40 +0200

    made first Row

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,12 @@ const int NUM_PIXELS = COLS * ROW;
 Adafruit_NeoPixel matrix_1 = Adafruit_NeoPixel(NUM_PIXELS, MATRIX_PIN_1, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel matrix_2 = Adafruit_NeoPixel(NUM_PIXELS, MATRIX_PIN_2, NEO_GBR + NEO_KHZ800);
 
+int firstRow[] = {20, 21, 26, 27, /*|*/ 18, 19, 28, 29,/*|*/ 16, 17, 30, 31,/*|*/ 
+                  352, 353, 366, 367,/*|*/ 354, 355, 365, 364,/*|*/ 356, 357, 362, 363, /*|*/
+                  361, 360, 358, 359};
+
+int secondRow[] = {};
+
 int joyX_1 = 0;
 int joyX_2 = 0;
 
@@ -24,6 +30,8 @@ void resetMatrix();
 void JoystickPosition();
 
 void setup() {
+    Serial.begin(9600);
+
     // Matrix_1 und Matrix_2 Initialisieren
     matrix_1.begin();
     matrix_1.setBrightness(BRIGHTNESS);
@@ -39,10 +47,25 @@ void setup() {
     // 8 und 9 auf Input stellen, um die Schalter einlesen zu können
     pinMode(8, INPUT);
     pinMode(9, INPUT);    
+    // A0 und A1 auf Input stellen, um die Joysticks einlesen zu können
+    pinMode(A0, INPUT);
+    pinMode(A1, INPUT);
 }
 
 void loop() {
+    joyX_1 = analogRead(A0);
+    joyX_2 = analogRead(A1);
     
+    if (joyX_1 > 520) {
+      for (int i = 0; i < 28; i++) {
+        matrix_1.setPixelColor(firstRow[i], 255, 10, 10);
+        matrix_2.setPixelColor(firstRow[i], 255, 10, 10);
+      }
+    } else if (joyX_1 < 500) {
+      
+    }
+    matrix_1.show();
+    matrix_2.show();
 }
  
 
@@ -91,5 +114,4 @@ void JoystickPosition() {
   
   joyX_1 = analogRead(A0);
   joyX_2 = analogRead(A1);
-
 }
